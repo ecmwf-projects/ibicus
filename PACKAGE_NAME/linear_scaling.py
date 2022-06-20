b13ac76830766c8d369e1b6f5e73f26e8875ac0c
jakobwes <jakobwes@aol.com>
2022-06-21 00:16:23 +0200

    Added extensive docstring to linear scaling

--- PACKAGE_NAME/linear_scaling.py
+++ PACKAGE_NAME/linear_scaling.py
@@ -1,3 +1,15 @@
+# (C) Copyright 1996- ECMWF.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+
+"""
+linear_scaling MODULE - implements debiasing using linear scaling.
+"""
+
 import warnings
 
 import numpy as np
@@ -13,7 +25,47 @@ standard_delta_types = {
 
 
 class LinearScaling(Debiaser):
-    def __init__(self, variable=None, delta_type=None):
+    """
+        Class LinearScaling representing debiasing via so-called linear scaling following Maraun 2016 as reference.
+
+        In linear scaling the present day model bias is either subtracted or divided from the future climate model time-series.
+        Let $y$ be the observed timeseries x_{hist}$ the simulated historical one and $x_{fut}$ the simulated future one (climate model historical and future run). Then additive linear scaling adjusts $x_fut$ as follows:
+
+    .. math::
+        x_{fut} \\rightarrow x_{fut} - (\\text{mean}(x_{hist}) - \\text{mean}(y))
+    ..raw:: html
+        and multiplicative scaling:
+
+    .. math::
+        x_{fut} \\rightarrow x_{fut} * \\frac{\\text{mean}(y)}{\\text{mean}(x_{hist})}.
+    ..raw:: html
+
+        Multiplicative scaling is hereby classically used for precipitation and additive scaling for temperature. Additive scaling amounts to a simple mean bias correction, whilst multiplicative one adjusts both mean and variance, but keeps their ration constant (Maraun 2016).
+
+
+        References:
+        Maraun, D. Bias Correcting Climate Change Simulations - a Critical Review. Curr Clim Change Rep 2, 211–220 (2016). https://doi.org/10.1007/s40641-016-0050-x
+
+        ...
+
+        Attributes
+        ----------
+        variable : str
+            Variable for which the debiasing is used
+        delta_type : str
+            One of ["additive", "multiplicative"]. Determines whether additive or multiplicative scaling is used.
+
+
+        Methods
+        -------
+        apply_location(obs: np.ndarray, cm_hist: np.ndarray, cm_future: np.ndarray) -> np.ndarray
+            Applies linear scaling at one location and returns the debiased timeseries.
+
+        apply(obs: np.ndarray, cm_hist: np.ndarray, cm_future: np.ndarray) -> np.ndarray
+            Applies linear scaling at all given locations on a grid and returns the the debiased timeseries.
+    """
+
+    def __init__(self, variable: str = None, delta_type: str = None):
 
         # Checks
         if variable is not None:
@@ -49,7 +101,9 @@ class LinearScaling(Debiaser):
                     "At least one of variable, delta_type needs to be specified."
                 )
 
-    def apply_location(self, obs, cm_hist, cm_future):
+    def apply_location(
+        self, obs: np.ndarray, cm_hist: np.ndarray, cm_future: np.ndarray
+    ) -> np.ndarray:
         if self.delta_type == "additive":
             return cm_future - (np.mean(cm_hist) - np.mean(obs))
         elif self.delta_type == "multiplicative":
