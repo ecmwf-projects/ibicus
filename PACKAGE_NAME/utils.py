45d541f0c5e31b23bb9200b8f91ea6dae27add6b
jakobwes <jakobwes@aol.com>
2022-06-23 12:53:40 +0200

    Added docstrings to all utils

--- PACKAGE_NAME/utils.py
+++ PACKAGE_NAME/utils.py
@@ -1,20 +1,24 @@
 import numpy as np
 
 
-def get_chunked_mean(x, n):
+def get_chunked_mean(x: np.ndarray, n: int) -> np.ndarray:
+    """Gets chunked means of an array. Splits an array in chunks of length n, calculates the mean in each chunk and returns an array of these values"""
     ids = np.arange(x.size) // n
     return np.bincount(ids, x) / np.bincount(ids)
 
 
-def get_yearly_mean(x, years):
+def get_yearly_mean(x: np.ndarray, years: np.ndarray) -> np.ndarray:
+    """Gets an array of yearly means of a timeseries x where each value has a corresponding year in years."""
     return np.array([np.mean(x[years == i_year]) for i_year in np.unique(years)])
 
 
-def threshold_cdf_vals(cdf_vals: np.ndarray, cdf_threshold: int = 0.0001):
+def threshold_cdf_vals(cdf_vals: np.ndarray, cdf_threshold: int = 0.0001) -> np.ndarray:
+    """Thresholds an array of cdf-values away from 0 and 1. Rounds down to `1-cdf_threshold` and up to `cdf_threshold` if above or below."""
     return np.maximum(np.minimum(cdf_vals, 1 - cdf_threshold), cdf_threshold)
 
 
-def interp_sorted_cdf_vals_on_given_length(cdf_vals: np.ndarray, interpolation_length: int):
+def interp_sorted_cdf_vals_on_given_length(cdf_vals: np.ndarray, interpolation_length: int) -> np.ndarray:
+    """Interpolates an array sorted cdf values onto a given length by linear interpolation."""
     return np.interp(
         np.linspace(1, cdf_vals.size, interpolation_length),
         np.linspace(1, cdf_vals.size, cdf_vals.size),
@@ -22,8 +26,8 @@ def interp_sorted_cdf_vals_on_given_length(cdf_vals: np.ndarray, interpolation_l
     )
 
 
-def sort_array_like_another_one(x, y):
-    """Sorts an array x like another one y, meaning that the biggest value of x is at the position of the biggest value of y, etc."""
+def sort_array_like_another_one(x: np.ndarray, y: np.ndarray) -> np.ndarray:
+    """Gets an array x into the order of another one y, meaning that the biggest value of x is at the position of the biggest value of y, etc."""
     inverse_sort_y = np.argsort(np.argsort(y))
     return np.sort(x)[inverse_sort_y]
 
