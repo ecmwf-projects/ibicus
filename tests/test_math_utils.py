c290035b8a32ebc671ad9734392b000e7a2844f9
jakobwes <jakobwes@aol.com>
2022-06-30 15:42:36 +0200

    Tests on iecdf and ecdf-functions with clearer names

--- tests/test_math_utils.py
+++ tests/test_math_utils.py
@@ -13,8 +13,9 @@ Demo tests for raw template.
 import unittest
 
 import numpy as np
+import scipy.stats
 
-from PACKAGE_NAME.utils import ecdf, iecdf
+from PACKAGE_NAME.utils import ecdf, gen_PrecipitationGammaLeftCensoredModel, iecdf
 
 
 class TestConsistencyOfIecdfandEcdfMethods(unittest.TestCase):
@@ -25,19 +26,19 @@ class TestConsistencyOfIecdfandEcdfMethods(unittest.TestCase):
         sorted_p = np.sort(cls.p)
         cls.min_distance = np.max(np.abs(sorted_p[0 : (sorted_p.size - 1)] - sorted_p[1:]))
 
-    def test_linear_interpolation(self):
+    def test_linear_interpolation_against_linear(self):
         assert all(
             np.abs(ecdf(self.x, iecdf(self.x, self.p, method="linear"), method="linear_interpolation") - self.p)
             < self.min_distance
         )
 
-    def test_step_function(self):
+    def test_step_function_against_inverted_cdf(self):
         assert all(
             np.abs(ecdf(self.x, iecdf(self.x, self.p, method="inverted_cdf"), method="step_function") - self.p)
             < self.min_distance
         )
 
-    def test_kernel_density(self):
+    def test_kernel_density_against_linear(self):
         assert all(
             np.abs(ecdf(self.x, iecdf(self.x, self.p, method="linear"), method="kernel_density") - self.p)
             < self.min_distance
@@ -59,3 +60,13 @@ class TestConsistencyOfIecdfandEcdfMethods(unittest.TestCase):
                 np.abs(ecdf(self.x, iecdf(self.x, self.p, method=iecdf_option), method="kernel_density") - self.p)
                 < self.min_distance
             )
+
+
+class Testgen_PrecipitationGammaLeftCensoredModel(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.params = (1, 0, 3)
+        cls.x = scipy.stats.gamma.rvs(*cls.params, size=1000)
+        cls.censoring_value = np.quantile(cls.x, 0.1)
+
+        cls.PrecipitationGammaLeftCensoredModel = gen_PrecipitationGammaLeftCensoredModel(cls.censoring_value)
