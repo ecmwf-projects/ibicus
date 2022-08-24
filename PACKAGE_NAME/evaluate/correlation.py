daf21f2b9ee40ba7f1d2c4ef5724aacdb627155d
jakobwes <jakobwes@aol.com>
2022-08-24 12:02:28 +0100

    Code review

--- PACKAGE_NAME/evaluate/correlation.py
+++ PACKAGE_NAME/evaluate/correlation.py
@@ -20,9 +20,8 @@ from PACKAGE_NAME.variables import *
 
 
 def rmse_spatial_correlation_distribution(variable: str, obs_data: np.ndarray, **cm_data) -> pd.DataFrame:
-
     """
-    Calculate Root-Mean-Squared-Error between observed and modelled spatial correlation matrix at each location.
+    Calculates Root-Mean-Squared-Error between observed and modelled spatial correlation matrix at each location.
 
     The computation involves the following steps: At each location, calculate the correlation to each other location in the observed as well as the climate model
     data set. Then calculate the mean squared error between these two matrices.
@@ -38,38 +37,32 @@ def rmse_spatial_correlation_distribution(variable: str, obs_data: np.ndarray, *
 
     """
 
-    rmsd_array = np.empty((0, 2))
-
+    rmsd_arrays = []
     for k in cm_data.keys():
 
         rmsd = np.zeros((obs_data.shape[1], obs_data.shape[2]))
 
-        for a in range(obs_data.shape[1]):
-            for b in range(obs_data.shape[2]):
+        for a, b in np.ndindex(obs_data.shape[1:]):
+            corr_matrix_obs = np.zeros((obs_data.shape[1], obs_data.shape[2]))
+            corr_matrix_cm = np.zeros((obs_data.shape[1], obs_data.shape[2]))
 
-                corr_matrix_obs = np.zeros((obs_data.shape[1], obs_data.shape[2]))
-                corr_matrix_cm = np.zeros((obs_data.shape[1], obs_data.shape[2]))
+            for i, j in np.ndindex(obs_data.shape[1:]):
+                corr_matrix_obs[i, j] = np.corrcoef(obs_data[:, a, b], obs_data[:, i, j])[0, 1]
+                corr_matrix_cm[i, j] = np.corrcoef(cm_data[k][:, a, b], cm_data[k][:, i, j])[0, 1]
 
-                for i in range(obs_data.shape[1]):
-                    for j in range(obs_data.shape[2]):
+            rmsd[a, b] = math.sqrt(sklearn.metrics.mean_squared_error(corr_matrix_obs, corr_matrix_cm))
 
-                        corr_matrix_obs[i, j] = np.corrcoef(obs_data[:, a, b], obs_data[:, i, j])[0, 1]
-                        corr_matrix_cm[i, j] = np.corrcoef(cm_data[k][:, a, b], cm_data[k][:, i, j])[0, 1]
+        rmsd_arrays.append(
+            pd.DataFrame(data={"Correction Method": [k] * rmsd.size, "RMSE spatial correlation": rmsd.flatten()})
+        )
 
-                rmsd[a, b] = math.sqrt(sklearn.metrics.mean_squared_error(corr_matrix_obs, corr_matrix_cm))
-
-        array = np.transpose(np.array([[k] * len(np.ndarray.flatten(rmsd)), np.transpose(np.ndarray.flatten(rmsd))]))
-
-        rmsd_array = np.append(rmsd_array, array, axis=0)
-
-    rmsd_data = pd.DataFrame(rmsd_array, columns=["Correction Method", "RMSE spatial correlation"])
+    rmsd_data = pd.concat(rmsd_arrays)
     rmsd_data["RMSE spatial correlation"] = pd.to_numeric(rmsd_data["RMSE spatial correlation"])
 
     return rmsd_data
 
 
 def rmse_spatial_correlation_boxplot(variable: str, dataset: pd.DataFrame):
-
     """
     Boxplot of RMSE of spatial correlation across locations.
 
@@ -78,7 +71,7 @@ def rmse_spatial_correlation_boxplot(variable: str, dataset: pd.DataFrame):
     variable : str
         Variable name, has to be given in standard form specified in documentation.
     dataset : pd.DataFrame
-        Ouput format of function rmse_spatial_correlation_distribution
+        Ouput format of function :py:func:`rmse_spatial_correlation_distribution`
 
     """
 
@@ -95,7 +88,6 @@ def rmse_spatial_correlation_boxplot(variable: str, dataset: pd.DataFrame):
 
 
 def calculate_multivariate_correlation_locationwise(variables, **kwargs):
-
     """
     Calculates correlation between two variables specified in keyword arguments at each location and outputs spatial plot.
 
@@ -120,10 +112,8 @@ def calculate_multivariate_correlation_locationwise(variables, **kwargs):
 
         correlation_matrix[k] = np.zeros((variable1.shape[1], variable1.shape[2]))
 
-        for i in range(variable1.shape[1]):
-            for j in range(variable1.shape[2]):
-
-                correlation_matrix[k][i, j] = np.corrcoef(variable1[:, i, j].T, variable2[:, i, j].T)[0, 1]
+        for i, j in np.ndindex(variable1.shape[1:]):
+            correlation_matrix[k][i, j] = np.corrcoef(variable1[:, i, j].T, variable2[:, i, j].T)[0, 1]
 
     axis_max = max(
         abs(max(np.ndarray.flatten(np.vstack(list(chain(*correlation_matrix.values())))))),
@@ -151,7 +141,6 @@ def calculate_multivariate_correlation_locationwise(variables, **kwargs):
 
 
 def define_multivariate_dataframes(variables: list, datasets_obs: list, datasets_bc: list, gridpoint=(0, 0)):
-
     """
     Convert 2d np.ndarrays into pd.DataFrame in order to use plot_direction_comparison_of_correlation_at_gridpoint, calculate_corr_bs_replicates and calculate_bs_replicates_mi
 
@@ -181,9 +170,7 @@ def define_multivariate_dataframes(variables: list, datasets_obs: list, datasets
 def plot_direct_comparison_of_correlation_at_gridpoint(
     variables: list, obs_dataframe: pd.DataFrame, bc_dataframe: pd.DataFrame
 ):
-
     """
-
     Uses function seaborn.regplot to plot scatterplot and Pearson correlation estimate of the two specified variables. Offers visual comparison of correlation at single location.
 
     Parameters
@@ -203,10 +190,8 @@ def plot_direct_comparison_of_correlation_at_gridpoint(
 
 
 def calculate_corr_bs_replicates(data: pd.DataFrame, size: int):
-
     """
-
-    Bootstrapping to obtain estimates of Pearson correlation coefficient in specified dataframe.
+    Bootstrapped estimates to obtain estimates of Pearson correlation coefficient in specified dataframe.
 
     Parameters
     ----------
@@ -235,9 +220,7 @@ def _calc_MI(x, y, bins):
 
 
 def calculate_bs_replicates_mi(data: pd.DataFrame, bins: int, size: int) -> np.ndarray:
-
     """
-
     Bootstrapping to obtain estimates of mutual information in specified dataframe.
 
     Parameters
